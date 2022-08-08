2367252285492d507e9a057953f97440d786baa2
Monisha Dhanasekar <97545391+Monishadhanasekar@users.noreply.github.com>
2022-08-08 20:45:35 +0530

    queue

---
+++ queue.js
@@ -0,0 +1,77 @@
+class Queue {
+    constructor() {
+        this.items = [];
+    }
+    
+    // add element to the queue
+    enqueue(element) {
+        return this.items.push(element);
+    }
+    
+    // remove element from the queue
+    dequeue() {
+        if(this.items.length > 0) {
+            return this.items.shift();
+        }
+    }
+
+    // front function
+    front()
+    {
+    // returns the Front element of 
+    // the queue without removing it.
+        if(this.isEmpty())
+             return "No elements in Queue";
+         return this.items[0];
+    }
+    
+    // view the last element
+    peek() {
+        return this.items[this.items.length - 1];
+    }
+    
+    // check if the queue is empty
+    isEmpty(){
+       return this.items.length == 0;
+    }
+
+    // printQueue function
+    printQueue()
+    {
+        var str = "";
+        for(var i = 0; i < this.items.length; i++)
+            str += this.items[i] +" ";
+        return str;
+    }
+   
+    // the size of the queue
+    size(){
+        return this.items.length;
+    }
+ 
+    // empty the queue
+    clear(){
+        this.items = [];
+    }
+}
+
+let queue = new Queue();
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(4);
+queue.enqueue(8);
+console.log(queue.items);
+
+queue.dequeue();
+console.log(queue.items);
+console.log(queue.front());
+console.log(queue.printQueue());
+
+console.log(queue.peek());
+
+console.log(queue.isEmpty());
+
+console.log(queue.size());
+
+queue.clear();
+console.log(queue.items);
